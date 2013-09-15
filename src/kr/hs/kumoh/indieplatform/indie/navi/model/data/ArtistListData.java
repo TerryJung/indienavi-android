0218f5d4dc0c1d00824339058edc1ab9d29f0718
yonghyunjung <chilchil@chilchil.me>
2013-09-16 02:34:16 +0900

    class make

---
+++ src/kr/hs/kumoh/indieplatform/indie/navi/model/data/ArtistListData.java
@@ -0,0 +1,12 @@
+package kr.hs.kumoh.indieplatform.indie.navi.model.data;
+
+public class ArtistListData {
+
+	String artistName;
+	String genre;
+	String label;
+	String artistText;
+	int debutYear;
+	int likeCnt;
+	String artistImgUrl;
+}
