bc83b24fc7f562632efbb78dcc558c558c6c5ff5
yonghyunjung <chilchil@chilchil.me>
2013-09-18 09:31:56 +0900

    fragment

--- src/kr/hs/kumoh/indieplatform/indie/navi/view/fragment/ArtistListFragment.java
+++ src/kr/hs/kumoh/indieplatform/indie/navi/view/fragment/ArtistListFragment.java
@@ -1,20 +1,41 @@
 package kr.hs.kumoh.indieplatform.indie.navi.view.fragment;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import kr.hs.kumoh.indieplatform.indie.navi.R;
+import kr.hs.kumoh.indieplatform.indie.navi.controller.net.JSONParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class ArtistListFragment extends Fragment {
+public class ArtistListFragment extends ListFragment {
+	
+	ArrayList<HashMap<String, String>> jsonlist = new ArrayList<HashMap<String, String>>();
+	ListView lv;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View root = inflater.inflate(R.layout.artist_fragment, container, false);
+//		new ProgressTask()
 		// TODO Auto-generated method stub
-		TextView tv = (TextView) root.findViewById(R.id.textView1);
+//		TextView tv = (TextView) root.findViewById(R.id.textView1);
 //		tv.setText("wwwww");
 		return root;
 	}
