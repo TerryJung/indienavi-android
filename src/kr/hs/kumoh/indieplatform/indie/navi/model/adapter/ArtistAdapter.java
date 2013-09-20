eb4dead01f3742357ea018602b75af5764bbac7a
yonghyunjung <chilchil@chilchil.me>
2013-09-20 10:53:52 +0900

    libs /  getView

--- src/kr/hs/kumoh/indieplatform/indie/navi/model/adapter/ArtistAdapter.java
+++ src/kr/hs/kumoh/indieplatform/indie/navi/model/adapter/ArtistAdapter.java
@@ -2,6 +2,8 @@ package kr.hs.kumoh.indieplatform.indie.navi.model.adapter;
 
 import kr.hs.kumoh.indieplatform.indie.navi.model.data.ArtistData;
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 public class ArtistAdapter extends ArrayAdapter<ArtistData> {
@@ -10,6 +12,13 @@ public class ArtistAdapter extends ArrayAdapter<ArtistData> {
 			ArtistData[] objects) {
 		super(context, resource, textViewResourceId, objects);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		
+		return super.getView(position, convertView, parent);
 	}
 
 }
