bdd5f7c085fb6407d1a493e9de7f20f0f8175d9f
yonghyunjung <chilchil@chilchil.me>
2013-09-21 00:52:21 +0900

    set adapter set data set listview layout

--- src/kr/hs/kumoh/indieplatform/indie/navi/model/adapter/ArtistAdapter.java
+++ src/kr/hs/kumoh/indieplatform/indie/navi/model/adapter/ArtistAdapter.java
@@ -1,24 +1,58 @@
 package kr.hs.kumoh.indieplatform.indie.navi.model.adapter;
 
+
+import java.util.ArrayList;
+
+import kr.hs.kumoh.indieplatform.indie.navi.R;
 import kr.hs.kumoh.indieplatform.indie.navi.model.data.ArtistData;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ArtistAdapter extends ArrayAdapter<ArtistData> {
+	
 
-	public ArtistAdapter(Context context, int resource, int textViewResourceId,
-			ArtistData[] objects) {
-		super(context, resource, textViewResourceId, objects);
+	private Context mContext;
+	private int artistResource;
+	private ArrayList<ArtistData> artistList;
+    private LayoutInflater artistInflater;
+
+    public ArtistAdapter(Context context, int resource, ArrayList<ArtistData> objects) {
+		super(context, resource, objects);
+		this.mContext = context;
+		this.artistResource = resource;
+		this.artistList = objects;
+		this.artistInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		// TODO Auto-generated constructor stub
 	}
+
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
+//		View v = convertView;
+		ArtistData artist = artistList.get(position);
 		
-		return super.getView(position, convertView, parent);
+		if(convertView == null)
+		{
+			convertView = artistInflater.inflate(artistResource, null);
+		}
+		// TODO Auto-generated method stub
+		ImageView artistImg = (ImageView) convertView.findViewById(R.id.artistImg);
+		TextView artistNameTv = (TextView) convertView.findViewById(R.id.artistName);
+		TextView labelNameTv = (TextView) convertView.findViewById(R.id.lableName);
+		TextView debutYearTv = (TextView) convertView.findViewById(R.id.debutYearTv);
+		TextView genreNameTv = (TextView) convertView.findViewById(R.id.genreName);
+		
+		
+		artistNameTv.setText(artist.getArtistName());
+		labelNameTv.setText(artist.getLabelName());
+		debutYearTv.setText(artist.getDebutYear());
+		genreNameTv.setText(artist.getGenreName());
+		return convertView;
 	}
 
 }
--- src/kr/hs/kumoh/indieplatform/indie/navi/model/data/ArtistData.java
+++ src/kr/hs/kumoh/indieplatform/indie/navi/model/data/ArtistData.java
@@ -1,9 +1,47 @@
 package kr.hs.kumoh.indieplatform.indie.navi.model.data;
 
 public class ArtistData {
-	String artistName;
-	String artistDesc;
-	String labelName;
-	int debutYear;
-	int likeCnt;
+	private String artistName;
+	private String artistDesc;
+	private String labelName;
+	private String debutYear;
+	private String genreName;
+	private int likeCnt;
+	
+	public String getArtistName() {
+		return artistName;
+	}
+	public void setArtistName(String artistName) {
+		this.artistName = artistName;
+	}
+	public String getArtistDesc() {
+		return artistDesc;
+	}
+	public void setArtistDesc(String artistDesc) {
+		this.artistDesc = artistDesc;
+	}
+	public String getLabelName() {
+		return labelName;
+	}
+	public void setLabelName(String labelName) {
+		this.labelName = labelName;
+	}
+	public String getDebutYear() {
+		return debutYear;
+	}
+	public void setDebutYear(String debutYear) {
+		this.debutYear = debutYear;
+	}
+	public void setGenreName(String genreName) {
+		this.genreName = genreName;
+	}
+	public String getGenreName() {
+		return genreName;
+	}
+	public int getLikeCnt() {
+		return likeCnt;
+	}
+	public void setLikeCnt(int likeCnt) {
+		this.likeCnt = likeCnt;
+	}
 }
