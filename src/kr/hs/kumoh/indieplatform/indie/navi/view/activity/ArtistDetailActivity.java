07e6c19ca49e9161404a2524ce5cd28adca8b1b6
yonghyunjung <chilchil@chilchil.me>
2013-09-24 21:47:54 +0900

    artistDetail  typing working complete

--- src/kr/hs/kumoh/indieplatform/indie/navi/view/activity/ArtistDetailActivity.java
+++ src/kr/hs/kumoh/indieplatform/indie/navi/view/activity/ArtistDetailActivity.java
@@ -1,25 +1,80 @@
 package kr.hs.kumoh.indieplatform.indie.navi.view.activity;
 
 import kr.hs.kumoh.indieplatform.indie.navi.R;
-import kr.hs.kumoh.indieplatform.indie.navi.R.layout;
-import kr.hs.kumoh.indieplatform.indie.navi.R.menu;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ArtistDetailActivity extends Activity {
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
+import com.androidquery.AQuery;
 
+public class ArtistDetailActivity extends SherlockFragmentActivity {
+
+	AQuery aq = new AQuery(this);
+	private ImageView artistImg;
+	private TextView artistName;
+	private TextView artistPan;
+	private TextView panTv;
+	private TextView labelName;
+	private TextView artistText;
+	private Button albumInfoBtn;
+	private Button concertInfoBtn;
+	private Button panclubBtn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_artist_detail);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		artistImg = (ImageView) findViewById(R.id.artistImg);
+		
+		artistName = (TextView) findViewById(R.id.artistName);
+		artistPan = (TextView) findViewById(R.id.artistPan);
+		panTv = (TextView) findViewById(R.id.PanTv);
+		labelName = (TextView) findViewById(R.id.labelName);
+		artistText = (TextView) findViewById(R.id.artistText);
+		albumInfoBtn = (Button) findViewById(R.id.albumInfoBtn);
+		albumInfoBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		concertInfoBtn = (Button) findViewById(R.id.albumInfoBtn);
+		concertInfoBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		panclubBtn = (Button) findViewById(R.id.albumInfoBtn);
+		panclubBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.artist_detail, menu);
-		return true;
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+        case android.R.id.home:
+            this.finish();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
 	}
-
 }
