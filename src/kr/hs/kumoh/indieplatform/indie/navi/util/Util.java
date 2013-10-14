ad39ccc607db31cdffc39623925466dd62e60a3c
yonghyunjung <chilchil@chilchil.me>
2013-10-14 17:34:08 +0900

    font add class

---
+++ src/kr/hs/kumoh/indieplatform/indie/navi/util/Util.java
@@ -0,0 +1,20 @@
+package kr.hs.kumoh.indieplatform.indie.navi.util;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+public class Util {
+
+	public final static void fontLight(Context c, TextView tv) {
+        tv.setTypeface(Typeface.createFromAsset(c.getAssets(), "NanumGothicLight.ttf"));
+    }
+
+    public final static void fontGeneral(Context c, TextView tv) {
+        tv.setTypeface(Typeface.createFromAsset(c.getAssets(), "NanumBarunGothic.ttf"));
+    }
+
+    public final static void fontBold(Context c, TextView tv) {
+        tv.setTypeface(Typeface.createFromAsset(c.getAssets(), "NanumBarunGothicBold.ttf"));
+    }
+}
